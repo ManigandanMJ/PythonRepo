f24f32826c4f4305b78108c1affe358f9546dbbe
ManigandanMJ <manigandan.mj@diggibyte.com>
2023-05-24 17:51:40 +0530

    updated logs to utils

--- src/second_max/utils.py
+++ src/second_max/utils.py
@@ -1,9 +1,15 @@
+import  logging
+
+logging.basicConfig(filename = "c:\\logs\\calendar_log.log",filemode="w")
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
 def second_max():
-    n = int(input())
+    log.info("Logged into second_max method")
     arr = map(int, input().split())
     max_arr = sorted(set(arr))
     print(max_arr)
     x = list(max_arr)
+    log.info("Found second runner up value")
     runner_up = (max_arr[-2])
     print(runner_up)
     return runner_up
