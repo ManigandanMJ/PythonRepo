639877de993678ef22cf63885f996294870ff8a2
ManigandanMJ <manigandan.mj@diggibyte.com>
2023-05-25 17:36:14 +0530

    updated test cases values

--- test/word_order_test/word_order_test.py
+++ test/word_order_test/word_order_test.py
@@ -1,10 +1,11 @@
 import unittest
-
+from src.word_order.utils import *
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
-
+        actual_input = len_word()
+        expected_output = 3
+        self.assertEqual(actual_input, expected_output)  # add assertion here
 
 if __name__ == '__main__':
     unittest.main()
