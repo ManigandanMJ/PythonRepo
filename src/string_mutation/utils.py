2008e19340dd5fae79bc1709e7ecb99cfc9467ad
ManigandanMJ <manigandan.mj@diggibyte.com>
2023-05-25 18:35:06 +0530

    This is utils file for string mutation

---
+++ src/string_mutation/utils.py
@@ -0,0 +1,8 @@
+
+def mutation(position,new_char):
+    old_str = 'abracadabra'
+    str_list = list(old_str)
+    str_list[int(position)] = new_char
+    str_new = ''.join(str_list)
+    print(str_new)
+    return str_new
