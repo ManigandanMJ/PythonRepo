5aba6f6a43e7ef6a61831f957504014682c379ae
ManigandanMJ <manigandan.mj@diggibyte.com>
2023-05-27 23:42:27 +0530

    updated loggers

--- src/string_mutation/utils.py
+++ src/string_mutation/utils.py
@@ -1,8 +1,15 @@
 # To change the tuple string to mutable
+import logging
+logging.basicConfig(filename = "c:\\logs\\str_mutation_log.log",filemode ='w',)
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
 def mutation(position,new_char):
+    log.info("Entered the funtion")
     old_str = 'abracadabra'
     str_list = list(old_str)
+    log.info("changed the position of old string to new char")
     str_list[int(position)] = new_char
+    log.info("join funtion used to join te strings")
     str_new = ''.join(str_list)
     print(str_new)
     return str_new
--- src/word_order/utils.py
+++ src/word_order/utils.py
@@ -1,19 +1,28 @@
 #To find the number of times the word is repeated
+import logging
+logging.basicConfig(filename = "c:\\logs\\word_order_log.log",filemode ='w',)
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
 num = int(input())
 words_list = []
 for i in range(num):
+    log.info("Appending the list values")
     words_list.append(input().strip())
 
 counts = {}
 
 for word in words_list:
+    log.info("Word is added to word deictionary")
     counts[word] = counts.get(word, 0) + 1
 
 def len_word():
+    log.info("Finding the length of dict")
     print(len(counts))
     return len(counts)
 def count_word():
     for word in words_list:
+        log.info("verifies if word in list")
         if counts[word] != 0:
             print(counts[word], end=" ")
             counts[word] = 0
