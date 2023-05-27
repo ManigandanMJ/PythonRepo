a36195de479d0cd97e2bafb5137e2104cc3ccf3f
ManigandanMJ <manigandan.mj@diggibyte.com>
2023-05-27 13:32:18 +0530

    This is test_case file for mean_var file

---
+++ test/nean_var_test/mean_var_test.py
@@ -0,0 +1,11 @@
+import unittest
+from  src.mean_var.utils import *
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        actual_input = mean_var()
+        expected_output = mean_var()
+        self.assertEqual(actual_input, expected_output)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
